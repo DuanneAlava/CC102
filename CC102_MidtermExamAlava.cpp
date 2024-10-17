9180e3d1c17dcdec42eda0c117eda00dafff833a
DuanneAlava <alavaduanneharold@gmail.com>
2024-10-17 09:28:46 +0800

    Update CC102_MidtermExamAlava.cpp

--- CC102_MidtermExamAlava.cpp
+++ CC102_MidtermExamAlava.cpp
@@ -5,8 +5,10 @@ int main() {
 	float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD;
 	float totalCostPhp, sugarCostPhp, riceCostPhp, sardinesCostPhp, coffeeCostPhp, milkCostPhp;
 	float sugarQty, sardinesQty, riceQty, coffeeQty, milkQTy;
-	float usdToPhp, poundToPhp;  //$1 to 57.44 pesos  //1p to 75.04 Philippine Peso
-	cout << "========= SEVENPM SARI-SARI STORE POINT OF SALE SYSTEM ============\n";
+	float usdToPhp, poundToPhp;  //$1 to 57.44 pesos  //1pound to 75.04 Philippine Peso
+	cout << "=====================================================================\n";
+	cout << "========= SEVENPM SARI-SARI STORE POINT OF SALE SYSTEM ==============\n";
+	cout << "=====================================================================\n";
 	//get user input a price for declared variables
 	cout << "Price of sugar: "; cin >> sugarPriceUSD;
 	cout << "Price of rice: "; cin >> ricePricePound;
@@ -19,17 +21,19 @@ int main() {
 	cout << "Quantity of sardines: "; cin >> sardinesQty;
 	cout << "Quantity of coffee: "; cin >> coffeeQty;
 	cout << "Quantity of milk: "; cin >> milkQTy;
-	// declared a conversion of dollars to peso
+	// declare a conversion of dollars to peso
 	cout << "The cost of one dollar in pesos: "; cin >> usdToPhp;
 	cout << "The cost of one pound in pesos: "; cin >> poundToPhp;
-	cout << "======================================================";
 	/* calcuate the cost each item in php
 	calculate the total cost in php*/
-	sugarCostPhp = sugarPriceUSD * sugarQty;
-	riceCostPhp = ricePricePound * riceQty;
-	sardinesCostPhp = sardinesPricePound * sardinesQty;
-	coffeeCostPhp = coffeePriceUSD * coffeeQty;
-	milkCostPhp = milkPriceUSD * milkQTy;
+	sugarCostPhp = (sugarPriceUSD * usdToPhp) * sugarQty;
+	riceCostPhp = (ricePricePound * poundToPhp) * riceQty;
+	sardinesCostPhp = (sardinesPricePound * poundToPhp) * sardinesQty;
+	coffeeCostPhp = (coffeePriceUSD * usdToPhp) * coffeeQty;
+	milkCostPhp = (milkPriceUSD * usdToPhp) * milkQTy;
 	totalCostPhp = sugarCostPhp + riceCostPhp + sardinesCostPhp + coffeeCostPhp + milkCostPhp;
+	cout << "=====================================================================\n";
+	cout << "The total cost in PHP: " << totalCostPhp << "php" << endl;
+	cout << "=====================================================================\n";
 	return 0;
 }
