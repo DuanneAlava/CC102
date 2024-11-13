43f8ea8206581a873ba6c20da0489759a50018b0
DuanneAlava <alavaduanneharold@gmail.com>
2024-11-13 08:30:27 +0800

    Add files via upload

---
+++ temperatureActivity01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int temperature;
+	cout << "Enter a temperature: ";
+	cin >> temperature;
+	if(temperature<32){
+		cout << "Bring heavy Jacket.. ";
+	}else if(temperature<=32&&50){
+		cout << "Bring Light Jacket.. ";
+	}else{
+		cout<<"No jacket Needed.. ";
+	} 
+	return 0;
+}
