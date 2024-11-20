ee659b22d3e6e90995d9d01561774d7b5b2aa23f
DuanneAlava <alavaduanneharold@gmail.com>
2024-11-20 08:59:11 +0800

    Update Act04Alava.cpp

--- Act04Alava.cpp
+++ Act04Alava.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main() {
     double balance, interestRate = 0.0;
-    string accountType, accountLevel;
+    char accountType, accountLevel;
     cout << "Enter your bank account balance: $"; 
-    cin >> balance;
-    cout << "Enter your account type (Personal or Business): "; 
-    cin >> accountType;
-    cout << "Enter your account level (Standard, Gold, or Platinum): "; 
-    cin >> accountLevel;
-    interestRate = (accountType == "Personal" && accountLevel == "Standard" && balance >= 0) ? 1.2 :
-                   (accountType == "Personal" && accountLevel == "Gold" && balance >= 1000) ? 1.9 :
-                   (accountType == "Personal" && accountLevel == "Gold" && balance >= 5000) ? 2.3 :
-                   (accountType == "Business" && accountLevel == "Standard" && balance >= 1500) ? 1.7 :
-                   (accountType == "Business" && accountLevel == "Platinum" && balance >= 10000) ? 2.5 : 0.0;
+	cin >> balance;
+    cout << "Enter your account type (P=Personal or B=Business): "; 
+	cin >> accountType;
+    cout << "Enter your account level (S=Standard, G=Gold, or P=Platinum): "; 
+	cin >> accountLevel;
+    interestRate = (accountType == 'P' || 'p' && accountLevel == 'S' || 's' && balance >= 0) ? 1.2 :
+                   (accountType == 'P' || 'p' && accountLevel == 'G' || 'g' && balance >= 1000) ? 1.9 :
+                   (accountType == 'P' || 'p' && accountLevel == 'G' || 'g' && balance >= 5000) ? 2.3 :
+                   (accountType == 'B' || 'b' && accountLevel == 'S' || 's' && balance >= 1500) ? 1.7 :
+                   (accountType == 'B' || 'b' && accountLevel == 'P' || 'p' && balance >= 10000) ? 2.5 : 0.0;
     cout << (interestRate > 0 ? "Your interest rate is: " + to_string(interestRate) + "%" :
                                 "Your account does not meet the minimum balance requirements for any interest rate.") << endl;
     return 0;
